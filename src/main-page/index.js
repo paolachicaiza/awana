767a88b3b9afe989833ca051c5fbf376c0d9071c
paolachicaiza <pao.paty.chicaiza@gmail.com>
2022-04-15 17:57:56 -0500

    Initial commit

---
+++ src/main-page/index.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './main-page.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div>
+      <Header>
+        
+      </Header>
+    </div>
+  );
+}
+
+export default App;
